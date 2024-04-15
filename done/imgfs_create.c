fe2aa4c5230d80fd2debf460d7794d64ce1040f8
zacharyydoll <dollzachary@gmail.com>
2024-04-15 13:55:22 +0200

    implemented do_create() in imgfs_create.c

---
+++ done/imgfs_create.c
@@ -0,0 +1,53 @@
+#include "imgfs.h"
+#include "error.h"
+#include <string.h> // for strncpy
+#include <stdlib.h> // for calloc
+
+int do_create(const char *imgfs_filename, struct imgfs_file *imgfs_file) {
+    // Parameter validity check
+    M_REQUIRE_NON_NULL(imgfs_file);
+    if (imgfs_filename == NULL) {
+        return ERR_INVALID_FILENAME;
+    }
+
+    // Open the file
+    imgfs_file->file = fopen(imgfs_filename, "wb");
+    if (!(imgfs_file->file)) {
+        return ERR_IO;
+    }
+
+    // Finish initialization of the header (max_files and resized_res are already provided)
+    strncpy(imgfs_file->header.name, CAT_TXT, sizeof(imgfs_file->header.name) - 1);
+    imgfs_file->header.name[sizeof(imgfs_file->header.name) - 1] = '\0'; // Ensure null-termination
+    imgfs_file->header.version = 1;
+    imgfs_file->header.nb_files = 0;
+    imgfs_file->header.unused_32 = 0;
+    imgfs_file->header.unused_64 = 0;
+
+    // Initialize all bytes of metadata to 0
+    imgfs_file->metadata = calloc(imgfs_file->header.max_files, sizeof(struct img_metadata));
+    if (imgfs_file->metadata == NULL) {
+        fclose(imgfs_file->file);
+        return ERR_OUT_OF_MEMORY;
+    }
+
+    // Handle write errors in the header
+    if (fwrite(&(imgfs_file->header), sizeof(struct imgfs_header), 1, imgfs_file->file) != 1) {
+        fclose(imgfs_file->file);
+        return ERR_IO;
+    }
+    // Handle write errors in the metadata
+    if (fwrite(imgfs_file->metadata,
+               sizeof(struct img_metadata),
+               imgfs_file->header.max_files,
+               imgfs_file->file) != imgfs_file->header.max_files) {
+
+        fclose(imgfs_file->file);
+        free(imgfs_file->metadata);
+        return ERR_IO;
+    }
+
+    printf("%d item(s) written\n", 1 + imgfs_file->header.nb_files); // header + each metadata entry
+
+    return ERR_NONE;
+}
