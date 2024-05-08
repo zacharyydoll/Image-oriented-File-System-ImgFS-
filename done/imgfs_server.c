53077af2675bf9bc37af28f11fa545652d441b09
zacharyydoll <dollzachary@gmail.com>
2024-05-08 14:09:58 +0200

    Implemented imgfs_server.c and imgfs_server_service.c

---
+++ done/imgfs_server.c
@@ -0,0 +1,59 @@
+/* 
+ * @file imgfs_server.c
+ * @brief ImgFS server part, main
+ *
+ * @author Konstantinos Prasopoulos
+ */
+
+#include "util.h"
+#include "imgfs.h"
+#include "socket_layer.h"
+#include "http_net.h"
+#include "imgfs_server_service.h"
+
+#include <string.h>
+#include <signal.h>
+#include <unistd.h>
+#include <stdlib.h> // abort()
+
+/********************************************************************/
+static void signal_handler(int sig_num _unused)
+{
+}
+
+/********************************************************************/
+static void set_signal_handler(void)
+{
+    struct sigaction action;
+    if (sigemptyset(&action.sa_mask) == -1) {
+        perror("sigemptyset() in set_signal_handler()");
+        abort();
+    }
+    action.sa_handler = signal_handler;
+    action.sa_flags   = 0;
+    if ((sigaction(SIGINT,  &action, NULL) < 0) ||
+        (sigaction(SIGTERM,  &action, NULL) < 0)) {
+        perror("sigaction() in set_signal_handler()");
+        abort();
+    }
+}
+
+/********************************************************************/
+
+int main (int argc, char *argv[])
+{
+    int err = server_startup(argc, argv);
+    if (err < 0) {
+        fprintf(stderr, "http_init() failed\n");
+        fprintf(stderr, "%s\n", ERR_MSG(err));
+        return err;
+    }
+
+    //loop on http_receive() as long as there are no error
+    while ((err = http_receive()) == ERR_NONE);
+    fprintf(stderr, "http_receive() failed\n");
+    fprintf(stderr, "%s\n", ERR_MSG(err));
+
+    server_shutdown();
+    return err;
+}
